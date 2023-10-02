e04e18225b808cbcaf014d03ab30b16b31854490
Muktikanta Mohapatra <mmuktikanta10@gmail.com>
2023-10-03 01:48:48 +0530

    Updated unit test utility class for UserResponseModel

--- src/main/java/com/shopping/user/model/response/UserResponseModel.java
+++ src/main/java/com/shopping/user/model/response/UserResponseModel.java
@@ -1,5 +1,6 @@
 package com.shopping.user.model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 //@Builder
 public class UserResponseModel {
     @NotNull
--- src/test/java/com/shopping/user/utility/RequestAndResponseFormationUtility.java
+++ src/test/java/com/shopping/user/utility/RequestAndResponseFormationUtility.java
@@ -5,7 +5,7 @@ import com.shopping.user.model.request.UserRequestModel;
 import com.shopping.user.model.response.UserResponseModel;
 
 public class RequestAndResponseFormationUtility {
-    public UserRequestModel requestModel=UserRequestModel.builder()
+    public UserRequestModel requestModel = UserRequestModel.builder()
             .alternateEmail("alternateEmail@gmail.com")
             .billingAddress("billing address")
             .email("dummy@gmail.com")
@@ -19,7 +19,7 @@ public class RequestAndResponseFormationUtility {
             .privacyPreferences(false)
             .userName("kanha111")
             .build();
-    public UserRequestModel requestModelWithValidationError=UserRequestModel.builder()
+    public UserRequestModel requestModelWithValidationError = UserRequestModel.builder()
             .alternateEmail("alternateEmail@gmail.com")
             .billingAddress("billing address")
             .email("emailgmail.com")
@@ -33,20 +33,8 @@ public class RequestAndResponseFormationUtility {
             .privacyPreferences(false)
             .userName("kanha111")
             .build();
-    public UserResponseModel responseModel=UserResponseModel.builder()
-            .billingAddress("billing address")
-            .email("dummy@gmail.com")
-            .currencyPreferences("INR")
-            .emailSubscription(false)
-            .languagePreferences("hindi oriya")
-            .password("12345")
-            .phoneNumber("12345")
-            .name("Kanha")
-            .shippingAddress("shipping address")
-            .privacyPreferences(false)
-            .userName("kanha111")
-            .build();
-    public UserEntity userEntity=UserEntity.builder()
+    public UserResponseModel responseModel = new UserResponseModel("kanha", "kanha111", "dummy@gmail.com", "12345", "shipping address", "billing address", "12345", "hindi oriya", "INR", false, false);
+    public UserEntity userEntity = UserEntity.builder()
             .alternateEmail("alternateEmail@gmail.com")
             .billingAddress("billing address")
             .email("dummy@gmail.com")
